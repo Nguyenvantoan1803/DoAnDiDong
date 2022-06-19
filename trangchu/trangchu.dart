d79bd68f2b03287fa6892380dd3afb56e31246b4
NguyenMinhDuc <checkthuanla@gmail.com>
2022-06-19 11:18:31 +0700

    Nhat update chuc nang bat sang toi

--- trangchu/trangchu.dart
+++ trangchu/trangchu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_application_1/src/dangnhap/basicTile.dart';
+import 'package:flutter_application_1/src/dangnhap/dangnhap.dart';
+import 'package:flutter_application_1/src/trangchu/dieuhuongphong.dart';
+import 'package:flutter_application_1/src/trangchu/doimatkhau.dart';
 
 class Trangchu extends StatefulWidget {
   const Trangchu({Key? key}) : super(key: key);
@@ -8,120 +9,130 @@ class Trangchu extends StatefulWidget {
   _TrangchuState createState() => _TrangchuState();
 }
 
-class _TrangchuState extends State<Trangchu> {
-  String urlImages = '';
-  final ha = ['images/ckc2.jpg'];
+bool _iconbool = false;
+IconData _iconlight = Icons.wb_sunny;
+IconData _icondart = Icons.nights_stay;
+ThemeData _lighttheme =
+    ThemeData(primarySwatch: Colors.amber, brightness: Brightness.light);
+ThemeData _darttheme = ThemeData(
+  primarySwatch: Colors.red,
+  brightness: Brightness.dark,
+);
 
+class _TrangchuState extends State<Trangchu> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Trang chủ'),
-        centerTitle: true,
-      ),
-    body: Column(children: [
-         
-          Container(
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: _iconbool ? _darttheme : _lighttheme,
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text('Trang chủ'),
+              centerTitle: true,
+              backgroundColor: Colors.blue,
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _iconbool = !_iconbool;
+                    });
+                  },
+                  icon: Icon(_iconbool ? _icondart : _iconlight),
+                ),
+              ]),
+          drawer: Drawer(
             child: ListView(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                children: <Widget>[
-              Text(
-                "Tin tức nỗi bật",
-                style: TextStyle(
-                  fontSize: 20,
-                  backgroundColor: Colors.blue,
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("Admin"),
+                  accountEmail: Text("admin@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 40.0),
+                    ),
+                  ),
                 ),
-              ),
-              ListTile(
-                title: Text('Phòng CTCT-HSSV'),
-                subtitle: Text("chương trình hiến máu nhân đạo"),
-                onTap: () {},
-              ),
-              Text(
-                "Tin tức xem nhiều nhất",
-                style: TextStyle(
-                  fontSize: 20,
-                  backgroundColor: Colors.blue,
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Phòng"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DieuhuongPhong()),
+                    );
+                  },
                 ),
-              ),
-              ListTile(
-                title: Text('Phòng đào tạo'),
-                subtitle: Text('Thời khóa biểu áp dụng ngày (05/02...'),
-                onTap: () {},
-              ),
-            ]))],)
-      
-      
-       Center(
-          child: CarouselSlider.builder(
-              options: CarouselOptions(
-                  height: 400,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 2)),
-              itemCount: urlImages.length,
-              itemBuilder: (context, index, realIndex) {
-                final urlImage = urlImages;
-                return buildImage(urlImage, index);
-              })),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Admin"),
-              accountEmail: Text("admin@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.orange,
-                child: Text(
-                  "A",
-                  style: TextStyle(fontSize: 40.0),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Khoa"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
+                ListTile(
+                  leading: Icon(Icons.contacts),
+                  title: Text("Thông tin cá nhân"),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Đổi mật khẩu"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Doimatkhau()),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.replay),
+                  title: Text("Đăng xuất"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DangNhap()),
+                    );
+                  },
+                ),
+              ],
             ),
-            // ListTile(
-            //   leading: Icon(Icons.home),
-            //   title: Text("Home"),
-            //   onTap: () {
-            //     Navigator.pop(context);
-            //   },
-            // ),
-            // ListTile(
-            //   leading: Icon(Icons.settings),
-            //   title: Text("Settings"),
-            //   onTap: () {
-            //     Navigator.pop(context);
-            //   },
-            // ),
-            // ListTile(
-            //   leading: Icon(Icons.contacts),
-            //   title: Text("Contact Us"),
-            //   onTap: () {
-            //     Navigator.pop(context);
-            //   },
-            // ),
-            ListView(
-              children: basictitles.map(buildTitle).toList(),
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+          body: Container(
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              children: <Widget>[
+                Text(
+                  "Tin tức nỗi bật",
+                  style: TextStyle(
+                    fontSize: 20,
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Phòng CTCT-HSSV'),
+                  subtitle: Text("chương trình hiến máu nhân đạo"),
+                  onTap: () {},
+                ),
+                Text(
+                  "Tin tức xem nhiều nhất",
+                  style: TextStyle(
+                    fontSize: 20,
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Phòng đào tạo'),
+                  subtitle: Text('Thời khóa biểu áp dụng ngày (05/02...'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ));
   }
-
-  Widget buildTitle(BasicTile title) {
-    return ExpansionTile(
-      title: Text(title.title),
-      children: title.titles.map((title) => buildTitle(title)).toList(),
-    );
-  }
-
-  Widget buildImage(String urlImage, int Index) => Container(
-      margin: EdgeInsets.symmetric(horizontal: 2),
-      color: Colors.green,
-      child: ListView(
-        children: [
-          Image.asset('images/ckc2.jpg '),
-        ],
-      ));
 }
