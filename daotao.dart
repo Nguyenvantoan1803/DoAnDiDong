0795097d9756e71bcc4edbda070df27be0dc3ab6
NguyenMinhDuc <checkthuanla@gmail.com>
2022-06-22 09:49:23 +0700

    Them giao dien dao tao

---
+++ daotao.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/dangnhap/dangnhap.dart';
+import 'package:flutter_application_1/src/trangchu/caidat.dart';
+import 'package:flutter_application_1/src/trangchu/dieuhuongphong.dart';
+import 'package:flutter_application_1/src/trangchu/doimatkhau.dart';
+import 'package:flutter_application_1/src/trangchu/trangchu.dart';
+
+class Daotao extends StatefulWidget {
+  @override
+  _DaoTaoState createState() => _DaoTaoState();
+}
+
+class _DaoTaoState extends State<Daotao> {
+  int _selectIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Đào tạo'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  showSearch(context: context, delegate: CustomSearch());
+                })
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("Admin"),
+                accountEmail: Text("admin@caothang.edu.vn"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    "A",
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Trang chính"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("Khoa CNTT"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("Đào tạo"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("CTCT-HSSV"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("Đoàn thanh niên"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("Học bổng- Vay vốn"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.account_balance),
+                title: Text("Học phí"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.replay),
+                title: Text("Đăng xuất"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DangNhap()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Lịch đào tạo"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Thời khóa biểu"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Lịch thi"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text("Kế hoạch tổ chức học kỳ phụ,học ghép"),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSearch extends SearchDelegate {
+  List<String> allData = [
+    'Phòng đào tạo',
+    'Đoàn thanh niên',
+    'Phòng hành chính-quản trị',
+    'Phòng CTCT-HSSV',
+    'Khoa CNTT',
+    'Học Bổng'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
